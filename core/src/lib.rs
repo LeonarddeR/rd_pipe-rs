ea0b9a35335687e2e12b8df2aab4f093d12d103b
Leonard de Ruijter <alderuijter@gmail.com>
2022-08-16 07:37:54 +0200

    Working code somehow

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod class_factory;
-pub mod pipe_manager;
 pub mod rd_pipe_plugin;
--- core/src/rd_pipe_plugin.rs
+++ core/src/rd_pipe_plugin.rs
@@ -1,8 +1,13 @@
-use std::sync::RwLock;
-
+use bytes::BytesMut;
+use core::slice;
+use std::{future, io::ErrorKind::WouldBlock, sync::Arc};
+use tokio::{
+    net::windows::named_pipe::{NamedPipeServer, PipeMode, ServerOptions},
+    sync::RwLock,
+};
 use tracing::{debug, error, info, instrument};
 use windows::{
-    core::{implement, Error, Result},
+    core::{implement, AgileReference, Error, Interface, Result},
     Win32::{
         Foundation::{BOOL, BSTR, E_UNEXPECTED},
         System::RemoteDesktop::{
@@ -15,22 +20,22 @@ use windows::{
 
 #[derive(Debug)]
 #[implement(IWTSPlugin)]
-pub struct RdPipePlugin(RwLock<Option<IWTSVirtualChannelManager>>);
+pub struct RdPipePlugin;
 
 impl RdPipePlugin {
     pub fn new() -> RdPipePlugin {
-        RdPipePlugin(RwLock::new(None))
+        RdPipePlugin
     }
 
     #[instrument]
     fn create_listener(
         &self,
         channel_mgr: &IWTSVirtualChannelManager,
-        channel_name: String,
+        channel_name: &str,
     ) -> Result<IWTSListener> {
         debug!("Creating listener with name {}", channel_name);
-        let callback: IWTSListenerCallback = RdPipeListenerCallback.into();
-        unsafe { channel_mgr.CreateListener((channel_name + "\0").as_ptr(), 0, &callback) }
+        let callback: IWTSListenerCallback = RdPipeListenerCallback::new(channel_name).into();
+        unsafe { channel_mgr.CreateListener(format!("{}\0", channel_name).as_ptr(), 0, &callback) }
     }
 }
 
@@ -44,7 +49,7 @@ impl IWTSPlugin_Impl for RdPipePlugin {
                 return Err(Error::from(E_UNEXPECTED));
             }
         };
-        self.create_listener(channel_mgr, "echo".to_string())?;
+        self.create_listener(channel_mgr, "echo")?;
         Ok(())
     }
 
@@ -69,7 +74,17 @@ impl IWTSPlugin_Impl for RdPipePlugin {
 
 #[derive(Debug)]
 #[implement(IWTSListenerCallback)]
-pub struct RdPipeListenerCallback;
+pub struct RdPipeListenerCallback {
+    pub name: String,
+}
+
+impl RdPipeListenerCallback {
+    pub fn new(name: &str) -> RdPipeListenerCallback {
+        RdPipeListenerCallback {
+            name: name.to_string(),
+        }
+    }
+}
 
 impl IWTSListenerCallback_Impl for RdPipeListenerCallback {
     #[instrument]
@@ -80,7 +95,10 @@ impl IWTSListenerCallback_Impl for RdPipeListenerCallback {
         pbaccept: *mut BOOL,
         ppcallback: *mut Option<IWTSVirtualChannelCallback>,
     ) -> Result<()> {
-        debug!("Creating new callback for channel {:?} with data {}", pchannel, data);
+        debug!(
+            "Creating new callback for channel {:?} with name {} and data {}",
+            pchannel, &self.name, data
+        );
         let channel = match pchannel {
             Some(c) => c.to_owned(),
             None => return Err(Error::from(E_UNEXPECTED)),
@@ -89,32 +107,93 @@ impl IWTSListenerCallback_Impl for RdPipeListenerCallback {
         let ppcallback = unsafe { &mut *ppcallback };
         *pbaccept = BOOL::from(true);
         debug!("Creating callback");
-        let callback: IWTSVirtualChannelCallback = RdPipeChannelCallback::new(channel).into();
+        let callback: IWTSVirtualChannelCallback =
+            RdPipeChannelCallback::new(channel, &self.name).into();
         debug!("Callback {:?} created", callback);
         *ppcallback = Some(callback);
         Ok(())
     }
 }
 
+const PIPE_NAME_PREFIX: &str = r"\\.\pipe\RdPipe";
+
 #[derive(Debug)]
 #[implement(IWTSVirtualChannelCallback)]
-pub struct RdPipeChannelCallback(IWTSVirtualChannel);
+pub struct RdPipeChannelCallback {
+    channel: IWTSVirtualChannel,
+    pipe_server: Arc<RwLock<NamedPipeServer>>,
+}
 
 impl RdPipeChannelCallback {
-    pub fn new(channel: IWTSVirtualChannel) -> RdPipeChannelCallback {
-        RdPipeChannelCallback(channel)
+    pub fn new(channel: IWTSVirtualChannel, channel_name: &str) -> RdPipeChannelCallback {
+        let addr = format!(
+            "{}_{}_{}",
+            PIPE_NAME_PREFIX,
+            channel_name,
+            channel.as_raw() as usize
+        );
+        let pipe_server = ServerOptions::new()
+            .max_instances(1)
+            .pipe_mode(PipeMode::Message)
+            .create(addr)
+            .unwrap();
+        let callback = RdPipeChannelCallback {
+            channel,
+            pipe_server: Arc::new(RwLock::new(pipe_server)),
+        };
+        callback.run_pipe_reader();
+        callback
+    }
+
+    fn run_pipe_reader(&self) {
+        let server = Arc::clone(&(self.pipe_server));
+        let channel = AgileReference::new(&(self.channel)).unwrap();
+        tokio::spawn(async move {
+            {
+                let reader = server.read().await;
+                reader.connect().await;
+            }
+            loop {
+                let mut buf = BytesMut::with_capacity(4096);
+                let reader = server.read().await;
+                match reader.try_read(&mut buf) {
+                    Ok(n) => {
+                        debug!("read {} bytes", n);
+                        let channel_inst = channel.resolve().unwrap();
+                        unsafe { channel_inst.Write(&mut buf, None) };
+                    }
+                    Err(e) if e.kind() == WouldBlock => {
+                        continue;
+                    }
+                    Err(e) => {
+                        return e;
+                    }
+                }
+            }
+        });
+    }
+}
+
+impl Drop for RdPipeChannelCallback {
+    fn drop(&mut self) {
+        self.OnClose();
     }
 }
 
 impl IWTSVirtualChannelCallback_Impl for RdPipeChannelCallback {
     #[instrument]
     fn OnDataReceived(&self, cbsize: u32, pbuffer: *const u8) -> Result<()> {
+        let slice = unsafe { slice::from_raw_parts(pbuffer, cbsize as usize) };
+        tokio::future::block_on(self.pipe_server.read())
+            .unwrap()
+            .try_write(slice);
         Ok(())
     }
 
     #[instrument]
     fn OnClose(&self) -> Result<()> {
-        debug!("Closing channel {:?} with callback {:?}", self.0, self);
-        unsafe { self.0.Close()}
+        debug!("Closing channel {}", self.channel.as_raw() as usize);
+        self.pipe_server.read().unwrap().disconnect().unwrap();
+        unsafe { self.channel.Close() }
     }
 }
